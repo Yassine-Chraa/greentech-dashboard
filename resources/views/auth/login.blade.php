ac609f80b8794eeeb69150433c9dff7ef9ae3fc7
Yassine-Chraa <yassine.chraa1@email.com>
2022-10-31 02:25:15 +0100

    ok

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,13 +8,13 @@
   <div class="row justify-content-center">
     <div class="col-md-8">
       <div class="card">
-        <div class="card-header">{{ __('Login') }}</div>
+        <div class="card-header">Connexion</div>
         <div class="card-body">
           <form method="POST" action="{{ route('login') }}">
             @csrf
 
             <div class="row mb-3">
-              <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+              <label for="email" class="col-md-4 col-form-label text-md-end">Email</label>
 
               <div class="col-md-6">
                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
@@ -41,7 +41,7 @@
               </div>
             </div>
 
-            <div class="row mb-3">
+            <!--<div class="row mb-3">
               <div class="col-md-6 offset-md-4">
                 <div class="form-check">
                   <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
@@ -51,17 +51,17 @@
                   </label>
                 </div>
               </div>
-            </div>
+            </div>-->
 
             <div class="row mb-0">
               <div class="col-md-8 offset-md-4">
                 <button type="submit" class="btn btn-primary">
-                  {{ __('Login') }}
+                  Se connecter
                 </button>
 
                 @if (Route::has('password.request'))
                 <a class="btn btn-link" href="{{ route('password.request') }}">
-                  {{ __('Forgot Your Password?') }}
+                  Vous avez oublié votre mot de passe?
                 </a>
                 @endif
               </div>
